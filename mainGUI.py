548b63b02324bb450ed0e241b1f7bfa26e5f3af4
Brody Howard <howbro@live.com>
2024-07-05 00:37:47 -0400

    v1.0
    Added final touches to GUI. Added program logging. Fixed custom graphs. Added return button so multiple runs can be done.

--- mainGUI.py
+++ mainGUI.py
@@ -1,23 +1,27 @@
 from _thread import *
 from customtkinter import *
 from queue import Queue
+from datetime import datetime
 from src.carina_parser import parser 
 from src.GUItools import tools
 
-class Carina_Plotter():
-    def __init__(self, Title: str, size: str = "900x500") -> None:
+
+class Carina_Plotter(CTk):
+    def __init__(self, Title: str) -> None:
+        super().__init__()
         self.queue = Queue()
-        self.window = CTk()
-        #self.window.geometry(size)
+        with open("program.log", "w") as file:
+            file.write(f'[T {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}] INFO: Program Started\n')
+            file.close()
         set_appearance_mode("dark")
         set_default_color_theme("blue")
-        self.window.title(Title)
+        self.title(Title)
         self.boot_screen()
-        self.window.mainloop()
+        self.mainloop()
         
     def boot_screen(self) -> None:
-        tools.clear_gui(self.window)
-        boot_frm = CTkFrame(master=self.window)
+        tools.clear_gui(self)
+        boot_frm = CTkFrame(master=self)
         greeting_lbl = CTkLabel(master=boot_frm, text="Welcome to the Carina Data Plotter", text_color="lightblue", font=("Arial", 30))
         prompt_frm = CTkFrame(master=boot_frm)
         prompt_lbl = CTkLabel(master=prompt_frm, text='Enter folder name which contains the data and events logs in a "\\raw" sub folder (Data\\_____\\raw):  ', font=("Arial", 16), anchor="center")
@@ -31,39 +35,37 @@ class Carina_Plotter():
         boot_frm.pack(pady=20, padx=30, fill="both", expand=True)
 
     def loading_screen(self, folder_name: str) -> None:
-        tools.clear_gui(self.window)
+        tools.clear_gui(self)
         self.folder_name = folder_name
         messages = ["Initalizing Parser", "Reading data.log", "Reading events.log", "Parsing Sensor Lines", "Parsing Actuator Lines", "Reformating Actuators Data and Converting to Dataframes", "Creating Graphs", "Complete"]
-        loading_frm = CTkFrame(master=self.window)
+        loading_frm = CTkFrame(master=self)
         loading_lbl = CTkLabel(master=loading_frm, text="Loading...", font=("Arial", 25))
         progress_bar = CTkProgressBar(master=loading_frm, orientation="horizontal", width = 190, height = 25)
         progress_bar.set(0)
         info_lbl = CTkLabel(master=loading_frm, text=messages[0], font=("Arial", 14))
         loading_lbl.pack(pady=15)
         progress_bar.pack(padx = 30,)
-        info_lbl.pack(pady=5)
+        info_lbl.pack(pady=5, padx=5)
         loading_frm.pack(padx=20, pady=30)
-        self.window.update()
+        self.update()
         start_new_thread(self.data_processor, (folder_name, ))
         progress = 0
         while progress < 0.85:
-            self.window.update()
             val = self.queue.get()
             progress = val/7
             progress_bar.set(progress)
-            loading_frm.children[list(loading_frm.children.keys())[-1]].destroy()
-            info_lbl = CTkLabel(master=loading_frm, text=messages[val], font=("Arial", 14))
-            info_lbl.pack(pady=5)
-            self.window.update()
+            info_lbl.configure(text=messages[val])
+            tools.append_to_log(messages[val], "INFO")
+            self.update()
         self.plot_all()
         progress_bar.set(1)
-        loading_frm.children[list(loading_frm.children.keys())[-1]].destroy()
-        info_lbl = CTkLabel(master=loading_frm, text=messages[7], font=("Arial", 14))
-        info_lbl.pack(pady=5)
-        self.window.update()
+        info_lbl.configure(text=messages[7])
+        self.update()
+        tools.append_to_log(f"Data Parsing and Plotting Complete", "INFO")
         self.data_screen()
 
     def data_processor(self, folder_name: str) -> None:
+        self.queue.put(0)
         parser.init(folder_name)
         self.queue.put(1)
         sensors, actuators = parser.parse_from_raw(self.queue)
@@ -75,6 +77,7 @@ class Carina_Plotter():
     def plot_all(self, start_time = 0, end_time = None) -> None:
         tools.generate_plots(self.folder_name, self.sensor_df, "sensor", start_time, end_time)
         tools.generate_plots(self.folder_name, self.actuator_df, "actuator", start_time, end_time)
+        tools.append_to_log(f'Generated {len(self.sensor_df.columns) + len(self.actuator_df.columns) - 2} Plots', "INFO")
 
     def custom_plot(self, xaxis_key: str, yaxis_key: str, start = 0, end = None):
         if xaxis_key in self.sensor_df.columns:
@@ -86,43 +89,45 @@ class Carina_Plotter():
         else:
             yaxis = (yaxis_key, self.actuator_df[yaxis_key].to_list())
         tools.single_plot(self.folder_name, xaxis, yaxis, start, end)
+        tools.append_to_log("Created a new custom plot", "INFO:")
             
     def data_screen(self) -> None:
-        tools.clear_gui(self.window)
+        tools.clear_gui(self)
 
-        self.window.grid_columnconfigure((0, 1, 2), weight=1)
-        self.window.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
-        title_frm = CTkFrame(master=self.window)
-        title_lbl = CTkLabel(master=title_frm, text="Data Plot Controller", font=("Arial", 30))
-        title_lbl.pack(pady=30, anchor="center", fill="both")
-        title_frm.grid(row = 0, column = 0, padx=10, pady=5, columnspan=3, sticky="ew")
+        self.grid_columnconfigure((0, 1, 2), weight=1)
+        self.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
+        title_frm = CTkFrame(master=self)
+        title_frm.grid_columnconfigure((0, 1), weight=1)
+        title_lbl = CTkLabel(master=title_frm, text="Data Plot Controller", text_color="lightblue", font=("Arial", 30))
+        title_lbl.grid(row = 0, column = 0, pady=30, columnspan=2, sticky="nsew")
+        title_frm.grid(row = 0, column = 0, padx=10, pady=5, columnspan=3, sticky="nsew")
 
-        replot_frm = CTkFrame(master=self.window)
+        replot_frm = CTkFrame(master=self)
         replot_frm.grid_columnconfigure((0, 1), weight=1)
         replot_frm.grid_rowconfigure((0, 1, 2, 3), weight=1)
         replot_lbl = CTkLabel(master=replot_frm, text="Replot All", font=("Arial", 22))
         replot_lbl.grid(row = 0, column = 0, padx=10, pady=10, columnspan=2, sticky="ew")
         start_time_lbl = CTkLabel(master=replot_frm, text="Start Time:", font=("Arial", 14))
         start_time_lbl.grid(row=1, column=0, pady=10, padx=(10, 5), sticky="ew")
-        start_time_ent = CTkEntry(master=replot_frm, font=("Arial", 14), width=80)
+        start_time_ent = CTkEntry(master=replot_frm, font=("Arial", 14), width=60)
         start_time_ent.grid(row=1, column=1, pady=10, padx=(0, 10), sticky="ew")
         end_time_lbl = CTkLabel(master=replot_frm, text="End Time:", font=("Arial", 14))
         end_time_lbl.grid(row=2, column=0, pady=10, padx=(10, 5), sticky="ew")
-        end_time_ent = CTkEntry(master=replot_frm, font=("Arial", 14), width=80)
+        end_time_ent = CTkEntry(master=replot_frm, font=("Arial", 14), width=60)
         end_time_ent.grid(row=2, column=1, pady=10, padx=(0, 10), sticky="ew")
-        replot_btn = CTkButton(master=replot_frm, text="Replot", font=("Arial", 18), anchor="center", command=tools.replot_caller(self.plot_all, start_time_ent, end_time_ent))
-        replot_btn.grid(row=3, column=0, columnspan=2, pady=20, padx=10, sticky="ew")
-        replot_frm.grid(row=1, column=0, padx=(10, 5), pady=(5, 10), rowspan=3, sticky="ew")
+        replot_btn = CTkButton(master=replot_frm, text="Replot", width=140, font=("Arial", 18), anchor="center", command=tools.replot_caller(self.plot_all, start_time_ent, end_time_ent))
+        replot_btn.grid(row=3, column=0, columnspan=2, pady=20, padx=10)
+        replot_frm.grid(row=1, column=0, padx=(10, 5), pady=(5, 10), rowspan=3, sticky="nsew")
 
-        custom_plot_frm = CTkFrame(master=self.window)
+        custom_plot_frm = CTkFrame(master=self)
         custom_plot_frm.grid_columnconfigure((0, 1, 2, 3), weight=1)
         custom_plot_frm.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
         cutom_plot_lbl = CTkLabel(master=custom_plot_frm, text="Create Custom Plot", font=("Arial", 22))
         cutom_plot_lbl.grid(row = 0, column = 0, padx=10, pady=10, columnspan=4, sticky="ew")
         start_lbl = CTkLabel(master=custom_plot_frm, text="Start:", font=("Arial", 16))
         start_lbl.grid(row=1, column=0, pady=10, padx=(10, 0), sticky="ew")
-        start__ent = CTkEntry(master=custom_plot_frm, font=("Arial", 16), width=60)
-        start__ent.grid(row=1, column=1, pady=10, padx=(0, 10), sticky="ew")
+        start_ent = CTkEntry(master=custom_plot_frm, font=("Arial", 16), width=60)
+        start_ent.grid(row=1, column=1, pady=10, padx=(0, 10), sticky="ew")
         end_lbl = CTkLabel(master=custom_plot_frm, text="End:", font=("Arial", 16))
         end_lbl.grid(row=1, column=2, pady=10, padx=(10, 0), sticky="ew")
         end_ent = CTkEntry(master=custom_plot_frm, font=("Arial", 16), width=60)
@@ -135,11 +140,41 @@ class Carina_Plotter():
         xaxis_opt.grid(row=3, column=0, padx=20, pady=5, columnspan=2, sticky="ew")
         yaxis_opt = CTkOptionMenu(master=custom_plot_frm, font=("Arial", 14), values=self.sensor_df.columns.to_list() + self.actuator_df.columns.to_list(), anchor="center")
         yaxis_opt.grid(row=3, column=2, padx=20, pady=5, columnspan=2, sticky="ew")
-        custom_plot_btn = CTkButton(master=custom_plot_frm, text="Create Plot", font=("Arial", 18), anchor="center", command=tools.custom_plot_caller(self.custom_plot, start_time_ent, end_time_ent, xaxis_opt, yaxis_opt)) # change command
-        custom_plot_btn.grid(row=4, column=1, columnspan=2, pady=20, padx=10, sticky="ew")
-        custom_plot_frm.grid(row=1, column=1, padx=(5, 10), pady=(5, 10), rowspan=4, columnspan=2, sticky="ew")
+        custom_plot_btn = CTkButton(master=custom_plot_frm, text="Create Plot", font=("Arial", 18), command=tools.custom_plot_caller(self.custom_plot, start_ent, end_ent, xaxis_opt, yaxis_opt)) # change command
+        custom_plot_btn.grid(row=4, column=1, columnspan=2, pady=(25, 20), sticky="ew")
+        custom_plot_frm.grid(row=1, column=1, padx=(5, 10), pady=(5, 10), rowspan=5, columnspan=2, sticky="nsew")
 
+        visual_switch = CTkSwitch(master=self, text="Visual Mode", command=self.switch_visual_mode)
+        visual_switch.grid(row=4, column=0, padx=10, pady=(0, 10), sticky="")
 
+        buttons_frm = CTkFrame(master=self)
+        buttons_frm.grid_columnconfigure((0, 1), weight=1)
+        back_btn = CTkButton(master=buttons_frm, text="Return", font=("Arial", 16), anchor="center", command=self.boot_screen)
+        back_btn.grid(row=0, column=0, pady=10, padx=(10, 5), sticky="nsew")
+        log_btn = CTkButton(master=buttons_frm, text="Logs", font=("Arial", 16), anchor="center", command=self.logs_screen)
+        log_btn.grid(row=0, column=1, pady=10, padx=(5, 10), sticky="nsew")
+        buttons_frm.grid(row=5, column=0, padx=5, pady=(0, 10), sticky="nsew")
+    
+    def logs_screen(self):
+        tools.clear_gui(self)
+        logs_lbl = CTkLabel(master=self, text="Program Log", font=("Arial", 20))
+        logs_lbl.grid(row=0, column=0, padx=10, pady=(20, 10), sticky="nsew")
+        return_btn = CTkButton(master=self, text="Return", font=("Arial", 18), command=self.data_screen)
+        return_btn.grid(row=0, column=2, padx=10, pady=(20, 10))
+        logs_txt = CTkTextbox(master=self, font=("Arial", 16), width=700)
+        logs_txt.grid(row=1, column=0, columnspan=3, rowspan=5, padx=10, pady=10, sticky="nsew")
+        with open("program.log", "r") as file:
+            for line in file:
+                logs_txt.insert("end", line)
+        logs_txt.configure(state="disabled")
+        logs_txt.see("end")
+
+    def switch_visual_mode(self):
+        if get_appearance_mode() == "Dark":
+            set_appearance_mode("light")
+        else:
+            set_appearance_mode("dark")
+        tools.append_to_log(f"Changed appearance mode to {get_appearance_mode()}", "INFO")
         
 if __name__ == "__main__":
     app = Carina_Plotter("Carina Data Proccesor")
--- src/GUItools/tools.py
+++ src/GUItools/tools.py
@@ -1,16 +1,20 @@
 from customtkinter import *
 from tkinter import messagebox
+from datetime import datetime
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import time as t
 
+
 def textbox_caller(func, text_box: CTkEntry):
     def call_func():
         text = text_box.get() + "\\"
-        if not os.path.exists("Data\\" + text + "raw\\data.log") or not os.path.exists("Data\\" + text + "raw\\events.log"):
+        folder = os.getcwd() + "\\Data\\" + text + "raw\\"
+        if not os.path.exists(folder + "data.log") or not os.path.exists(folder + "events.log"):
             gui_error("File Path not Found")
             return
+        append_to_log(f'Begining data parsing in {folder}', "INFO")
         func(text)
     return call_func
 
@@ -22,21 +26,22 @@ def replot_caller(func, start_box: CTkEntry, end_box: CTkEntry):
             if start == "" and end == "":
                 func()
             elif start == "":
-                func(end=int(end))
+                func(end=float(end))
             elif end == "":
-                func(start=int(start))
+                func(start=float(start))
             else:
-                start = int(start)
-                end = int(end_box)
+                start = float(start)
+                end = float(end)
                 if start > end:
                     gui_error("End time cannot be less then Start time")
+                    append_to_log(f"Failed to create graphs as start time was greater then end time (start:{start}, end:{end})", "ERROR")
                     return
                 func(start, end)
         except Exception:
             gui_error("Invalid Start or End Time")
     return call_func2
 
-def custom_plot_caller(func, start_box: CTkEntry, end_box: CTkEntry, option1: CTkOptionMenu, option2: CTkOptionMenu): # debug 
+def custom_plot_caller(func, start_box: CTkEntry, end_box: CTkEntry, option1: CTkOptionMenu, option2: CTkOptionMenu): 
     def call_func2():
         try:
             start = start_box.get()
@@ -44,33 +49,36 @@ def custom_plot_caller(func, start_box: CTkEntry, end_box: CTkEntry, option1: CT
             if start == "" and end == "":
                 func(option1.get(), option2.get())
             elif start == "":
-                func(option1.get(), option2.get(), end=int(end))
+                func(option1.get(), option2.get(), end=float(end))
             elif end == "":
-                func(option1.get(), option2.get(), start=int(start))
+                func(option1.get(), option2.get(), start=float(start))
             else:
-                start = int(start)
-                end = int(end_box)
+                start = float(start)
+                end = float(end)
                 if start > end:
-                    gui_error("End time cannot be less then Start time")
+                    gui_error("End cannot be less then Start")
+                    append_to_log(f"Failed to create graphs as start time was greater then end time (start:{start}, end:{end})", "ERROR")
                     return
                 func(option1.get(), option2.get(), start, end)
         except Exception:
-            gui_error("Invalid Start or End Time")
+            gui_error("Invalid Start or End value")
     return call_func2
 
 def gui_error(msg: str) -> None:
     messagebox.showerror(title="Program Error", message=msg)
+    append_to_log(msg, "ERROR")
 
 def clear_gui(window: CTk) -> None:
     for child in window.children.copy():
-        window.children[child].destroy()  
+        window.children[child].destroy() 
+    append_to_log("Clearing GUI Screen", "INFO") 
 
 def single_plot(folder_name: str, xaxis: tuple, yaxis: tuple, start = 0, end = None) -> None:
     if not os.path.exists("Data\\" + folder_name + "Plots"):
         os.mkdir("Data\\" + folder_name + "Plots")
 
-    start_ind = get_time_index(xaxis[1], start)
-    end_ind = get_time_index(xaxis[1], end)
+    start_ind = get_xaxis_index(xaxis[1], start)
+    end_ind = get_xaxis_index(xaxis[1], end)
     plt.plot(xaxis[1][start_ind:end_ind], yaxis[1][start_ind:end_ind])
     plt.title(f"{yaxis[0]} vs {xaxis[0]} Plot")
     plt.xlabel(xaxis[0])
@@ -83,8 +91,8 @@ def generate_plots(folder_name: str, dataframe: pd.DataFrame, type: str = "senso
         os.mkdir("Data\\" + folder_name + "Plots")
 
     time = dataframe["Time"].to_list()
-    start = get_time_index(time, start_time)
-    end = get_time_index(time, end_time)
+    start = get_xaxis_index(time, start_time)
+    end = get_xaxis_index(time, end_time)
     time = time[start:end]
     if type.lower() == "actuator":
         time.append(time[-1] + 0.01)
@@ -102,13 +110,21 @@ def generate_plots(folder_name: str, dataframe: pd.DataFrame, type: str = "senso
             plt.savefig(f"Data\\{folder_name}Plots\\{column}_[T{t.strftime('%Hh%Mm%Ss', t.gmtime(start_time))};T{t.strftime('%Hh%Mm%Ss', t.gmtime(end_time))}].jpg")
             plt.close()
 
-def get_time_index(time: list, given_time) -> int:
+def get_xaxis_index(xaxis: list, given_time) -> int:
     if given_time == 0:
         return given_time
     elif given_time is None:
-        return len(time)
+        return len(xaxis)
     else:
-        for i, t in enumerate(time):
+        for i, t in enumerate(xaxis):
             if t > given_time:
                 return i - 1
+            
+def append_to_log(msg: str, mode: str = 'info') -> None:
+    try:
+        with open("program.log", "a") as file:
+            file.write(f'[T {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}] {mode}: {msg}\n')
+            file.close()
+    except Exception:
+        gui_error("Error adding to program log")
 
