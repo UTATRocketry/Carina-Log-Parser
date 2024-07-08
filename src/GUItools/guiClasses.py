31a6be5f30f54a8b17e23b498d11637d096ba5dc
Brody Howard <howbro@live.com>
2024-07-08 09:10:45 -0400

    Yaxis Data Remove Button
    Added a remove button to the GUI so if you add to many yaxis data options or you need to make a different graph you can remove some.

--- src/GUItools/guiClasses.py
+++ src/GUItools/guiClasses.py
@@ -2,20 +2,24 @@ from typing import Any, Tuple
 from customtkinter import *
 
 class OptionsBar(CTkFrame):
-    def __init__(self, *args, master: Any, titles: list = [], choices: list = [], command = None, padx:int = 0, pady:int = 0, font:tuple = ("Arial", 16), **kwargs)->None:
+    def __init__(self, *args, master: Any, titles: list = [], choices: list = [], addcommand = None, removecommmand = None, padx:int = 0, pady:int = 0, font:tuple = ("Arial", 16), **kwargs)->None:
         super().__init__(*args, master=master, **kwargs)
 
-        self.num_boxes = len(titles)
+        self.num_boxes = len(titles) + 2
         self.choices = choices
         self.padx = padx
         self.pady = pady
         self.font = font
         self.titles = titles
         self.option_boxes = []
-        if command is None:
-            self.command = self.add_box
+        if addcommand is None:
+            self.addcommand = self.add_box
         else:
-            self.command = command
+            self.addcommand = addcommand
+        if removecommmand is None:
+            self.removecommand = self.remove_box
+        else:
+            self.removecommand = removecommmand
 
         self.grid_columnconfigure(self.get_column_tuple(), weight=1)
         self.grid_rowconfigure((0, 1), weight=1)
@@ -25,27 +29,41 @@ class OptionsBar(CTkFrame):
             t.grid(row=0, column=i, padx=self.padx, pady=self.pady, sticky="nsew")
             self.last_title = t
 
-        for i in range(self.num_boxes):
+        for i in range(self.num_boxes - 2):
             opt = CTkOptionMenu(master=self, font=self.font, values=self.choices, anchor="center")
-            opt.grid(row=1, column=i, padx=10, pady=5, sticky="ew")
+            opt.grid(row=1, column=i, padx=5, pady=5, sticky="ew")
             self.option_boxes.append(opt)
 
-        self.button = CTkButton(master=self, text="+", font=self.font, width = 20, command=self.command)
-        self.button.grid(row=1, column=self.num_boxes, sticky="ew")
+        self.addbutton = CTkButton(master=self, text="+", font=self.font, width = 30, command=self.addcommand)
+        self.addbutton.grid(row=1, column=self.num_boxes - 2, padx=5, pady=5, sticky="ew")
+        self.removebutton = CTkButton(master=self, text="-", font=self.font, width = 30, fg_color="darkred", hover_color="red", command=self.removecommand)
+        self.removebutton.grid(row=1, column=self.num_boxes - 1, padx=5, pady=5, sticky="ew")
 
     def add_box(self)->None:
         new_opt = CTkOptionMenu(master=self, font=self.font, values=self.choices, anchor="center")
-        new_opt.grid(row=1, column=self.num_boxes, padx=10, pady=5, sticky="ew")
+        new_opt.grid(row=1, column=self.num_boxes - 2, padx=10, pady=5, sticky="ew")
         self.option_boxes.append(new_opt)
         self.num_boxes += 1
         self.grid_columnconfigure(self.get_column_tuple(), weight=1)
-        self.button.grid(row=1, column=self.num_boxes, sticky="ew")
+        self.addbutton.grid(row=1, column=self.num_boxes - 2, padx=5, pady=5, sticky="ew")
+        self.removebutton.grid(row=1, column=self.num_boxes - 1, padx=5, pady=5, sticky="ew")
         self.last_title.grid(row=0, column=len(self.titles) - 1, columnspan=self.num_boxes, padx=self.padx, pady=self.pady, sticky="nsew")
         self.update()
 
+    def remove_box(self):
+        if self.num_boxes - 2 > len(self.titles):
+            last_box = self.option_boxes.pop()
+            last_box.destroy()
+            self.num_boxes -= 1
+            self.grid_columnconfigure(self.get_column_tuple(), weight=1)
+            self.addbutton.grid(row=1, column=self.num_boxes - 2, padx=5, pady=5, sticky="ew")
+            self.removebutton.grid(row=1, column=self.num_boxes - 1, padx=5, pady=5, sticky="ew")
+            self.last_title.grid(row=0, column=len(self.titles) - 1, columnspan=self.num_boxes, padx=self.padx, pady=self.pady, sticky="nsew")
+            self.update()
+
     def get_column_tuple(self) -> tuple:
         res = ()
-        for i in range(self.num_boxes + 1):
+        for i in range(self.num_boxes):
             res = (*res, i)
         return res
 
